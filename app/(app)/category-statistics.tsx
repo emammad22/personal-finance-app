182d89079bd4b9ee1ea1b43a46a89a94d258c21a
emammad22 <mammadoveltun22@gmail.com>
2025-04-01 16:02:53 +0400

    config endpoints for pie chart page

--- app/(app)/category-statistics.tsx
+++ app/(app)/category-statistics.tsx
@@ -3,20 +3,35 @@ import React from "react";
 import { ArrowLeft, Fuel } from "lucide-react-native";
 import PieChart from "react-native-pie-chart";
 import { router } from "expo-router";
+import { useCategoryStatics } from "@/features/home/queries/use-category-statistics";
+import { categoryColors, transactionIcons } from "@/constants/icon";
+import { Slice } from "react-native-pie-chart";
 
 const CategoryStatistics = () => {
-  const series = [
-    { value: 430, color: "#fbd203", },
-    { value: 321, color: "#ffb300" },
-    { value: 185, color: "#ff9100" },
-    { value: 123, color: "#ff6c00" },
-  ];
+  const categoryStatisticsQuery = useCategoryStatics();
+  const statistics = categoryStatisticsQuery.data;
+
+  const series =
+    statistics?.categories.reduce((acc, category) => {
+      if (category.total_amount) {
+        acc.push({
+          value: category.total_amount,
+          color: categoryColors?.[category.category_name as keyof typeof categoryColors],
+        });
+      }
+      return acc;
+    }, [] as Pick<Slice, "value" | "color">[]) || [];
+
+  const TOTAL_TRANSACTIONS = statistics?.categories?.reduce((acc, category) => {
+    acc += category.transaction_count;
+    return acc;
+  }, 0);
 
   return (
     <SafeAreaView className="flex-1">
       <View className="flex flex-col gap-5 p-5">
         <View className="flex flex-row justify-between items-center">
-          <TouchableOpacity onPress={()=>router.replace('/(app)/(home)/history')}>
+          <TouchableOpacity onPress={() => router.replace("/(app)/(home)/history")}>
             <ArrowLeft size={30} />
           </TouchableOpacity>
           <Text className="text-[20px]">Expenses</Text>
@@ -31,27 +46,35 @@ const CategoryStatistics = () => {
             </View>
           </View>
           <View className="flex flex-col justify-center items-center relative">
-            <PieChart  series={series} widthAndHeight={300} cover={{ radius: 0.7, color: "white" }} padAngle={0.04} />
+            <PieChart series={series} widthAndHeight={300} cover={{ radius: 0.7, color: "white" }} padAngle={0.04} />
             <View className="absolute top-[40%] left-[30%] flex flex-col gap-4 items-center">
               <Text className="text-[20px]">All transactions</Text>
-              <Text className="text-[16px] text-[#000]/20">26 transactions</Text>
+              <Text className="text-[16px] text-[#000]/20">{TOTAL_TRANSACTIONS} transactions</Text>
             </View>
           </View>
         </View>
         {/* Common info about each category transactions */}
         <View className="bg-[#eaeaea] p-6 rounded-[20px]">
-          <View className="flex flex-row justify-between">
-            <View className="flex flex-row gap-6 items-center">
-              <Fuel size={30} />
-              <View className="flex flex-col gap-3">
-                <Text>YDM</Text>
-                <Text>2 transactions</Text>
-              </View>
-            </View>
-            <View className="flex flex-col gap-3">
-              <Text>35.00$</Text>
-              <Text>33.02%</Text>
-            </View>
+          <View className="flex flex-col gap-5">
+            {statistics?.categories?.map((category, idx) => {
+              return (
+                <View className="flex flex-row justify-between">
+                  <View className="flex flex-row gap-6 items-center">
+                    <View className="p-3 bg-slate-400 rounded-[10px]">
+                      {transactionIcons?.[category.category_name as keyof typeof transactionIcons]({ size: 30 })}
+                    </View>
+                    <View className="flex flex-col gap-3">
+                      <Text className="text-[18px]">{category.category_name}</Text>
+                      <Text className="text-[13px]">{category.transaction_count} transactions</Text>
+                    </View>
+                  </View>
+                  <View className="flex flex-col gap-3 items-end">
+                    <Text className="text-[18px]">{category.total_amount} $</Text>
+                    <Text>{category.percentage} %</Text>
+                  </View>
+                </View>
+              );
+            })}
           </View>
         </View>
       </View>
